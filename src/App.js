ecbe663cc08c416b1a2762dea9298c15c3109472
komedy2205 <komedy2205@gmail.com>
2022-02-01 19:33:45 +0200

    friends

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { ProfileName } from './components/Profile/ProfileData/ProfileData';
 import user from './components/Profile/UserData/user.json';
-import { StatisticData } from './components/Statistics/components/StatisticData/StatisticData';
-import data from './components/Statistics/components/Data/Data.json';
+import { StatisticData } from './components/Statistics/StatisticData/StatisticData';
+import data from './components/Statistics/Data/Data.json';
 import { FriendListItem } from './components/FriendList/FriendList/FriendList';
 import friend from './components/FriendList/Data/friends.json';
 
