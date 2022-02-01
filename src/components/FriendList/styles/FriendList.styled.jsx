e52b7eec8fff3ce6986da51a15b6e21f663e8ea4
komedy2205 <komedy2205@gmail.com>
2022-02-01 19:29:46 +0200

    friends

--- src/components/FriendList/styles/FriendList.styled.jsx
+++ src/components/FriendList/styles/FriendList.styled.jsx
@@ -1,21 +1,5 @@
 import styled from '@emotion/styled';
 
-// export const Container = styled.div`
-//     background-color: lightgrey;
-
-//     width: auto;
-//     height: 600px;
-    
-//     padding-top: 150px;
-//     /* padding-left: ; */
-
-//     margin-right: auto;
-//     margin-left: auto;
-
-//     text-align: center;
-// `;
-
-
 export const List = styled.ul`
     list-style: none;
     display: block;
